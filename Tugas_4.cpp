bca81258c1e14ddd24179f0710f59953da38358c
elramaa <ramaelmaulanaagung@gmail.com>
2024-09-03 22:07:07 +0700

    tugas 4 solved

--- Tugas_4.cpp
+++ Tugas_4.cpp
@@ -3,11 +3,20 @@
 using namespace std;
 
 int main(){
-	vector<vector<int>> jarak = {
-		{0, },
-		{10, 0},
-		{15, 8, 0},
-		{20, 10, 5, 0}
-	};	
+	vector<vector<int>> jarak{
+		{0},
+		{10},
+		{15, 8},
+		{20, 10, 5}
+	};
+	
+	int kota1, kota2, kota3;
+	cin >> kota1 >> kota2 >> kota3;	
+	int temp = kota2;
+	if(kota1 < kota2) swap(kota1, kota2);
+	if(temp < kota3) swap(temp, kota3);
+	int jarak1 = (kota1 != kota2 ? jarak[kota1][kota2] : 0);
+	int jarak2 = (temp != kota3 ? jarak[temp][kota3] : 0);
+	cout << jarak1 + jarak2;
 	return 0;
 }
