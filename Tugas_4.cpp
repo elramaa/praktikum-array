fc5eb57cdefd86f0219313abdf280a2216e5c760
Rama El <75620554+elramaa@users.noreply.github.com>
2024-09-06 10:00:58 +0700

    Update Tugas_4.cpp

--- Tugas_4.cpp
+++ Tugas_4.cpp
@@ -24,19 +24,22 @@ int main(){
 	cout << "Masukkan kota tujuan 2: ";
 	cin >> kota3;
 
+	printf("Jarak kota %c -> %c -> %c = ", kota[kota1], kota[kota2], kota[kota3]);
+	
 	// [kota1][kota2] bernilai sama dengan [kota2][kota1]
 	// Sehingga untuk mengakses [kota2][kota1] dapat dilakukan swap untuk mengakses indeks yang tidak ada pada vektor
 	// Misal, kita tidak dapat mengakses [0][3] tapi bisa mengakses [3][0] sehingga kita dapat menukarnya apabila kota1 lebih kecil dari kota2
 	int temp = kota2;
 	if(kota1 < kota2) swap(kota1, kota2);
 	if(temp < kota3) swap(temp, kota3);
-
+	
 	// Apabila kedua kota merupakan kota yang sama, maka jaraknya adalah 0
 	int jarak1 = (kota1 != kota2 ? jarak[kota1][kota2] : 0);
 	int jarak2 = (temp != kota3 ? jarak[temp][kota3] : 0);
+	int totalJarak = jarak1 + jarak2;
+
 
 	// Tampilkan hasil penjumlahan jarak
-	int totalJarak = jarak1 + jarak2;
-	printf("Jarak kota %c -> %c -> %c = %d", kota[kota1], kota[kota2], kota[kota3], totalJarak);
+    	printf("%d", totalJarak);
 	return 0;
 }
